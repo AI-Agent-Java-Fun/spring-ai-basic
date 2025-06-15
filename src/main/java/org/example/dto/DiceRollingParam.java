2c9fb128a32f83bec84b74aa685e7a09b6d2a678
Egor Pozdnov <pozdner98@gmail.com>
2025-06-16 00:50:28 +0300

    Added log

---
+++ src/main/java/org/example/dto/DiceRollingParam.java
@@ -0,0 +1,20 @@
+package org.example.dto;
+
+public class DiceRollingParam {
+    private Integer count;
+
+    public DiceRollingParam() {
+    }
+
+    public DiceRollingParam(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
--- src/main/java/org/example/tool/ToolExample.java
+++ src/main/java/org/example/tool/ToolExample.java
@@ -1,5 +1,6 @@
 package org.example.tool;
 
+import org.example.dto.DiceRollingParam;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,8 @@ import org.springframework.stereotype.Component;
 public class ToolExample {
 
     @Tool(description = "Инструмент для бросание игральных кубиков, который возвращает сумму выпавших очков")
-    String diceRollingTool(@ToolParam(description = "How many dice to roll") Integer count) {
+    String diceRollingTool(@ToolParam(description = "specify the number of dice to roll. Specify it in json format with the count field.") DiceRollingParam diceRollingParam) {
+        System.out.println(diceRollingParam.getCount());
         return "5";
     }
 }
